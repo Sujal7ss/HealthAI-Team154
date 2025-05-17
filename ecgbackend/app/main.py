ec6a032349a1f4524b3b3e17a00dbf0c4dd71be7
Sujal7ss <sujalchahande3@gmail.com>
2025-05-18 04:38:02 +0530

    ECG model backend integrated with FastAPI

--- ecgbackend/app/main.py
+++ ecgbackend/app/main.py
@@ -1,15 +1,10 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import uvicorn
-import tensorflow as tf
-import numpy as np
-from PIL import Image
-import io
-import gradio as gr
+from api.routes import router
 
 app = FastAPI()
 
-# Enable CORS for frontend communication
+# Enable CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -18,47 +13,5 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Load the Keras model
-model = tf.keras.models.load_model("mlmodel/ecg_model.keras", compile=False)
-
-# Class labels
-class_labels = [
-    "Left Bundle Branch Block",
-    "Normal",
-    "Premature Atrial Contraction",
-    "Premature Ventricular Contractions",
-    "Right Bundle Branch Block",
-    "Ventricular Fibrillation"
-]
-
-# Preprocess function
-def preprocess_image(image_bytes):
-    img = Image.open(io.BytesIO(image_bytes)).convert("RGB")
-    img = img.resize((224, 224))
-    img_array = np.array(img) / 255.0
-    img_array = np.expand_dims(img_array, axis=0)
-    return img_array
-
-# Gradio wrapper
-def predict_from_gradio(img):
-    img_array = np.array(img.resize((224, 224))) / 255.0
-    img_array = np.expand_dims(img_array, axis=0)
-    prediction = model.predict(img_array)
-    predicted_class = class_labels[np.argmax(prediction)]
-    return predicted_class
-
-demo = gr.Interface(fn=predict_from_gradio, inputs=gr.Image(type="pil"), outputs="text")
-
-@app.get("/")
-def gradio_ui():
-    return demo.launch(share=False, inline=True, inbrowser=True)
-
-# Prediction endpoint
-@app.post("/predict/")
-async def predict_ecg(file: UploadFile = File(...)):
-    print("Received file:", file.filename)
-    contents = await file.read()
-    img_array = preprocess_image(contents)
-    prediction = model.predict(img_array)
-    predicted_class = class_labels[np.argmax(prediction)]
-    return {"prediction": predicted_class}
+# Include API router
+app.include_router(router)
